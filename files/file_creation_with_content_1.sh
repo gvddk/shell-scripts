020d4092e2ff9ee516686708515ec1d48873a27f
Deekshit <dixithnitw@gmail.com>
2022-07-19 10:05:59 +0530

    Pushing file creation scripts.

---
+++ files/file_creation_with_content_1.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/sh
+########################################################################################################################
+#### This program creates a text file with sample content.
+######################################################################################################################## 
+file="/tmp/test.txt"
+echo "Hello, I am Deekshit" >$file
+echo "Working as a software engineer" >>$file
+echo "Company Name is - Adobe" >>$file
+
